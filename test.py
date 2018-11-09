f63fbb42851c10856672cc93d0c256a3f4b94283
wbrm77 <wbrm77@gmail.com>
2018-11-09 12:39:47 -0500

    create function to search for type and color

--- test.py
+++ test.py
@@ -37,41 +37,9 @@ def drawBox(car, ax):
     plt.text(x, y, label)
 
 
-def findObject(atr):
-    headers = {"Content-type": "application/json",
-               "X-Access-Token": "nikyJuVbPcrjvx2W7A1ijY76V7uBpGRXNpTA"}
-    conn = httplib.HTTPSConnection("dev.sighthoundapi.com",
-           context=ssl.SSLContext(ssl.PROTOCOL_TLSv1))
-
-    image = 'maxresdefault_live.jpg'
-
-    # To use a hosted image uncomment the following line and update the URL
-    #image_data = "http://example.com/path/to/hosted/image.jpg"
-
-    # To use a local file uncomment the following line and update the path
-    image_data = base64.b64encode(open(image, "rb").read()).decode()
-
-    # api call
-    params = json.dumps({"image": image_data})
-    conn.request("POST", "/v1/recognition?objectType=vehicle,licenseplate", params, headers)
-    response = conn.getresponse()
-    result = response.read()
-
-    # parse json
-    my_json = result.decode('utf8').replace("'", '"')
-    data = json.loads(my_json)
-    s = json.dumps(data, indent=4, sort_keys=True)
-
-    im = np.array(Image.open(image), dtype=np.uint8)
-
-    # Create figure and axes
-    fig,ax = plt.subplots(1)
-
-    # Display the image
-    ax.imshow(im)
-
+def findAny(atr, cars):
     # draw bounding box
-    for car in data['objects']:
+    for car in cars:
         color = car['vehicleAnnotation']['attributes']['system']['color']['name']
         make = car['vehicleAnnotation']['attributes']['system']['make']['name']
         model = car['vehicleAnnotation']['attributes']['system']['model']['name']
@@ -79,7 +47,51 @@ def findObject(atr):
         if atr.lower() == color.lower() or atr.lower() == make.lower() or atr.lower() == model.lower() or atr.lower() == type.lower():
             drawBox(car, ax)
 
-    plt.show()
-    # print(s)
 
-findObject('ford')
+def findTypeColor(type, color, cars):
+    # draw bounding box
+    for car in cars:
+        car_color = car['vehicleAnnotation']['attributes']['system']['color']['name']
+        car_type = car['vehicleAnnotation']['attributes']['system']['vehicleType']
+        if color.lower() == car_color.lower() and type.lower() == car_type.lower():
+            drawBox(car, ax)
+
+
+
+headers = {"Content-type": "application/json",
+           "X-Access-Token": "nikyJuVbPcrjvx2W7A1ijY76V7uBpGRXNpTA"}
+conn = httplib.HTTPSConnection("dev.sighthoundapi.com",
+       context=ssl.SSLContext(ssl.PROTOCOL_TLSv1))
+
+image = 'maxresdefault_live.jpg'
+
+# To use a hosted image uncomment the following line and update the URL
+#image_data = "http://example.com/path/to/hosted/image.jpg"
+
+# To use a local file uncomment the following line and update the path
+image_data = base64.b64encode(open(image, "rb").read()).decode()
+
+# api call
+params = json.dumps({"image": image_data})
+conn.request("POST", "/v1/recognition?objectType=vehicle,licenseplate", params, headers)
+response = conn.getresponse()
+result = response.read()
+
+# parse json
+my_json = result.decode('utf8').replace("'", '"')
+data = json.loads(my_json)
+s = json.dumps(data, indent=4, sort_keys=True)
+
+im = np.array(Image.open(image), dtype=np.uint8)
+
+# Create figure and axes
+fig,ax = plt.subplots(1)
+
+# Display the image
+ax.imshow(im)
+
+# findAny('ford', data['objects'])
+findTypeColor('suv', 'white', data['objects'])
+
+plt.show()
+# print(s)
