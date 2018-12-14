2d78cdefdcef6ac688611938296dc4e66a990600
wbrm77 <wbrm77@gmail.com>
2018-12-14 15:44:35 -0500

    finish script to crop and label each picture

--- cropImages.py
+++ cropImages.py
@@ -9,41 +9,53 @@ def crop(image_path, coords, saved_location):
     @param saved_location: Path to save the cropped image
     """
     image_obj = Image.open(image_path)
+    width, height = image_obj.size
     cropped_image = image_obj.crop(coords)
     cropped_image.save(saved_location)
     # cropped_image.show()
 
-
-with open("JSON-Data/00000012.json", "r") as f:
-        data = json.load(f)
-
-keys = list(data.keys())
-
-x = data[keys[0]]['regions']['0']['shape_attributes']['x']
-y = data[keys[0]]['regions']['0']['shape_attributes']['y']
-height = data[keys[0]]['regions']['0']['shape_attributes']['height']
-width = data[keys[0]]['regions']['0']['shape_attributes']['width']
-#print(data)
-print(json.dumps(data, indent=4, sort_keys=True))
-
-
-# for ele in data:
-#     print(data['region_attributes'])
-
-#loop over all JSON
+#loop over all JSONs
 #loop over each picture
 #loop over each box
 
 def select_region():
-    for j in range(0, len(keys)):
-        for i in range(0, len(data[keys[j]]['regions'])):
-            filename = data[keys[j]]['filename']
+    labels = {}
+    index = 0
+    for k in range(1, 19):
+        with open("JSON-Data/" + str(k).zfill(8) + ".json", "r") as f:
+                data = json.load(f)
 
-            x1 = data[keys[j]]['regions'][str(i)]['shape_attributes']['x']
-            y1 = data[keys[j]]['regions'][str(i)]['shape_attributes']['y']
-            y2 = y1 + data[keys[j]]['regions'][str(i)]['shape_attributes']['height']
-            x2 = x1 + data[keys[j]]['regions'][str(i)]['shape_attributes']['width']
+        keys = list(data.keys())
 
-            crop('./images/00000012/' + filename, (x1, y1, x2, y2), 'output/' + filename + str(i) + '.jpg')
+        for j in range(0, len(keys)):
+
+            for i in range(0, len(data[keys[j]]['regions'])):
+                filename = data[keys[j]]['filename']
+
+                if data[keys[j]]['regions'][str(i)]['shape_attributes']['width'] == 0 or data[keys[j]]['regions'][str(i)]['shape_attributes']['height'] == 0:
+                    continue
+
+                x1 = data[keys[j]]['regions'][str(i)]['shape_attributes']['x']
+                y1 = data[keys[j]]['regions'][str(i)]['shape_attributes']['y']
+                y2 = y1 + data[keys[j]]['regions'][str(i)]['shape_attributes']['height']
+                x2 = x1 + data[keys[j]]['regions'][str(i)]['shape_attributes']['width']
+
+                labels[str(index)] = []
+                labels[str(index)].append(data[keys[j]]['regions'][str(i)]['region_attributes'])
+
+                # print('width = ' + str(data[keys[j]]['regions'][str(i)]['shape_attributes']['width']))
+                # print('height = ' + str(data[keys[j]]['regions'][str(i)]['shape_attributes']['height']))
+                # print('x1 = ' + str(x1))
+                # print('y1 = ' + str(y1))
+                # print('x2 = ' + str(x2))
+                # print('y2 = ' + str(y2))
+
+                # print(json.dumps(data, indent=4, sort_keys=True))
+
+                crop('./Traffic_Dataset/' + str(k).zfill(8) + '/' + filename, (x1, y1, x2, y2), 'output/' + str(index) + '.jpg')
+                index += 1
+
+    with open('label.txt', 'w') as outfile:
+        json.dump(labels, outfile)
 
 select_region()
